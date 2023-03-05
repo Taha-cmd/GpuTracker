e6af6a571de40881f13056d1b25196f921ea0f43
Taha <taharaed98@gmail.com>
2023-03-05 13:48:36 +0100

    add kafka helm chart

---
+++ GpuTracker.Producer/Program.cs
@@ -0,0 +1,26 @@
+﻿// https://docs.confluent.io/kafka-clients/dotnet/current/overview.html#ak-dotnet
+
+using Confluent.Kafka;
+
+var config = new ProducerConfig()
+{
+    BootstrapServers = "localhost:9094"
+};
+
+using var producer = new ProducerBuilder<string, string>(config).Build();
+
+for (int i = 10; i < 100; i++)
+{
+    var message = new Message<string, string>()
+    {
+        Key = "key-test" + i,
+        Value = "leitner-k8s-mastre" + i
+    };
+
+    producer.Produce("Gpus", message, report =>
+    {
+        Console.WriteLine(report.Status);
+    });
+}
+
+producer.Flush();
