02cb5a9227af8b9cc72957829d4c1e078d18d4c6
Grengster <gregor.zeman@gmx.at>
2023-03-16 15:17:19 +0100

    changed 10m to 1m

--- Kafka/Deploy-KafkaHelmChart.ps1
+++ Kafka/Deploy-KafkaHelmChart.ps1
@@ -1,5 +1,5 @@
 #Requires -Version 7.2
 
 # helm dependency build
-helm upgrade gpu-tracker "$PSScriptRoot\..\GpuTrackerHelmChart\" --atomic --namespace "gpu-tracker" --create-namespace --install --timeout 10m
+helm upgrade gpu-tracker "$PSScriptRoot\..\GpuTrackerHelmChart\" --atomic --namespace "gpu-tracker" --create-namespace --install --timeout 1m
 # kubectl port-forward service/kafka-ui 8080:8080 --namespace=kafka
