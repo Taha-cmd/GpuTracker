869e1c0b6ddd8a5488e3b5835015f5c2addc0d1e
Grengster <gregor.zeman@gmx.at>
2023-03-16 15:16:19 +0100

    idk what i did

--- Kafka/Deploy-KafkaHelmChart.ps1
+++ Kafka/Deploy-KafkaHelmChart.ps1
@@ -1,5 +1,5 @@
 #Requires -Version 7.2
 
 # helm dependency build
-helm upgrade gpu-tracker "$PSScriptRoot\..\GpuTrackerHelmChart\" --atomic --namespace "gpu-tracker" --create-namespace --install --timeout 1m
+helm upgrade gpu-tracker "$PSScriptRoot\..\GpuTrackerHelmChart\" --atomic --namespace "gpu-tracker" --create-namespace --install --timeout 10m
 # kubectl port-forward service/kafka-ui 8080:8080 --namespace=kafka
