c2fdb6912e4193d35be46bc019d9fe2bf7806b73
Taha <taharaed98@gmail.com>
2023-03-26 17:34:22 +0200

    change helm chart values structure

--- Deploy.ps1
+++ Deploy.ps1
@@ -9,9 +9,3 @@ $valuesFile = "$PSScriptRoot\Deployment\$Mode-values.yaml"
 
 # helm dependency build
 helm upgrade -f $valuesFile gpu-tracker "$PSScriptRoot\GpuTrackerHelmChart\" --atomic --namespace "gpu-tracker" --create-namespace --install --timeout 5m
-
-# if ($Mode -eq "Debug") {
-#     kubectl port-forward service/kafka-ui 8080:8080 --namespace=kafka
-#     # kubectl port-forward service/
-# }
-
