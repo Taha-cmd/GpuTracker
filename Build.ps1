e1e10fc68654f02e68f7b9b817250f3948e56392
Taha <taharaed98@gmail.com>
2023-04-16 12:47:15 +0200

    parallelize build script

--- Build.ps1
+++ Build.ps1
@@ -1,5 +1,14 @@
 #Requires -Version 7.2
 
-& Build/Build-DockerImage -Component "App" -Tag "gpu-tracker-app"
-& Build/Build-DockerImage -Component "Consumer" -Tag "gpu-tracker-consumer"
-& Build/Build-DockerImage -Component "Producer" -Tag "gpu-tracker-producer"
+# map component name to tag
+$private:dockerImages = @{
+    App      = "gpu-tracker-app"
+    Consumer = "gpu-tracker-consumer"
+    Producer = "gpu-tracker-producer"
+}
+
+# build the images in parallel to save time
+$dockerImages.keys | ForEach-Object -Parallel {
+    $tags = $Using:dockerImages # Using is required to capture variables from the outer scope in -Parallel foreach
+    & Build/Build-DockerImage -Component $_ -Tag $tags[$_]
+}
