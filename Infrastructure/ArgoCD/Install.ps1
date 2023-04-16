2c4b75ee51b36461c73d822cdeb9a3f166596d15
Taha <taharaed98@gmail.com>
2023-04-16 11:28:40 +0200

    add argo cd installer

---
+++ Infrastructure/ArgoCD/Install.ps1
@@ -0,0 +1,15 @@
+#Requires -Version 7.2
+
+# basic and quick installation guide: https://argo-cd.readthedocs.io/en/stable/getting_started/
+# the installation can be further customized using the community maintained helm chart: https://github.com/argoproj/argo-helm/tree/main/charts/argo-cd
+# there is a core installation that does not include the ui https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/core-install.yaml
+
+$private:installationManifestUrl = "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml"
+
+kubectl create namespace argocd | Out-Null
+kubectl apply -n argocd -f $installationManifestUrl | Out-Null
+
+$private:passwordBase64 = kubectl get secrets/argocd-initial-admin-secret -n argocd -o template='{{.data.password}}'
+[Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($passwordBase64)) | Out-Host
+
+
