e6881f5cbb1f76f909acfa9ca99773003a32a49f
Taha <taharaed98@gmail.com>
2023-04-16 11:29:34 +0200

    change deployment directory structure

--- Deploy.ps1
+++ Deploy.ps1
@@ -5,7 +5,7 @@ param (
     [Parameter(Mandatory)][ValidateSet("Debug", "Kubernetes")][string]$Mode
 )
 
-$valuesFile = "$PSScriptRoot\Deployment\$Mode-values.yaml"
+$valuesFile = "$PSScriptRoot\Deployment\values\$Mode-values.yaml"
 
 # helm dependency build
-helm upgrade -f $valuesFile gpu-tracker "$PSScriptRoot\GpuTrackerHelmChart\" --atomic --namespace "gpu-tracker" --create-namespace --install --timeout 5m
+helm upgrade -f $valuesFile gpu-tracker "$PSScriptRoot\Deployment\HelmChart\" --atomic --namespace "gpu-tracker" --create-namespace --install --timeout 5m
